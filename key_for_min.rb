b2a99ca96428fb5f5e336394a3843923c003c8d9
Brittani Taylor <brittani.elan.taylor@gmail.com>
2020-09-10 23:30:56 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,9 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  key, value = name_hash.first
+  name_hash.each do |k, v|
+    key = k if v < value
+  end
+  key
 end
